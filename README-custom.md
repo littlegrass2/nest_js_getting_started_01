77ff784d36980ab783c14fa1de2c38f532e2367a
Gg <zhengguanglou@gmail.com>
2025-10-01 15:22:17 +1000

    customise header by using express-Response

---
+++ README-custom.md
@@ -0,0 +1,41 @@
+
+## NestJs Getting Started Project
+
+
+### Project Initialisation - 09 2025 - points
+
+```bash
+$ npm i -g @nestjs/cli
+$ npx nest new nest_js_getting_started_01
+
+```
+
+To create a module using the CLI, simply execute the commands:
+
+```bash
+$ nest g module cats
+$ nest g controller cats
+$ nest g service cats
+
+```
+
+### Project setup
+
+```bash
+$ npm install
+```
+
+### Compile and run the project
+
+```bash
+# development
+$ npm run start
+
+# watch mode
+$ npm run start:dev
+
+# production mode
+$ npm run start:prod
+```
+
+
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { CatsService } from './cats.service';
+import type { Response } from 'express';
 
 @Controller('cats')
 export class CatsController {
@@ -8,5 +9,14 @@ export class CatsController {
   @Get('hello')
   getHello(): string {
     return this.catsService.getHello();
+  }
+
+  @Get('all')
+  findAll(@Res() res: Response) {
+    // console.log('Request:', request);
+    console.log('Response:', res);
+    // return 'This action returns all cats';
+    res.header('Custom-Header', 'NestJS - 001122');
+    res.status(201).send('This action returns all cats 201');
   }
 }
